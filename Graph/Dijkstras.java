21a5b5d0175733fbf8dc67fc4a9ee66fbb8c0ba5
onkar oulkar <onkaroulkar@gmail.com>
2024-11-09 10:06:38 +0530

    new code

--- Graph/Dijkstras.java
+++ Graph/Dijkstras.java
@@ -24,15 +24,15 @@ public class Dijkstras {
         graph[0].add(new Edge(0,1,2));
         graph[0].add(new Edge(0,2,4));
 
-        graph[0].add(new Edge(1,3,7));
-        graph[0].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,3,7));
+        graph[1].add(new Edge(1,2,1));
 
-        graph[0].add(new Edge(2,4,3));
+        graph[2].add(new Edge(2,4,3));
 
-        graph[0].add(new Edge(3,5,1));
+        graph[3].add(new Edge(3,5,1));
 
-        graph[0].add(new Edge(4,3,2));
-        graph[0].add(new Edge(4,5,5));
+        graph[4].add(new Edge(4,3,2));
+        graph[4].add(new Edge(4,5,5));
     }
 
     public static class Pair implements Comparable<Pair>{
